85022675fafb91b08fdd08b087dd266ec00e8b29
Anirban Mandal <53831400+AnirbanM13@users.noreply.github.com>
2026-02-22 11:40:17 +0530

    Update sim-3-data-gen.R

--- code/sim-3-data-gen.R
+++ code/sim-3-data-gen.R
@@ -3,7 +3,7 @@ library(Rcpp)
 
 Sys.setenv("PKG_CXXFLAGS"="-O3 -march=native -fopenmp")
 Sys.setenv("PKG_LIBS"="-fopenmp")
-sourceCpp("toy-openMP-2.cpp")
+sourceCpp("sim-1-2-3-data-gen-engine.cpp")
 
 N.main <- 2*1e5
 al.vec <- c(rep(1,N.main/2), rep(2,N.main/2))
